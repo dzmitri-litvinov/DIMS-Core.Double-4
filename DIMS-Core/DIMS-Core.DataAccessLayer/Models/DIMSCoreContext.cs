3d045cc89ec1c43a449e3d2ec9a7256f775fa0ea
dzmitri-litvinov <dzmitri.litvinov@gmail.com>
2021-07-01 16:51:48 +0300

    Revert entities

--- DIMS-Core/DIMS-Core.DataAccessLayer/Models/DIMSCoreContext.cs
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Models/DIMSCoreContext.cs
@@ -1,12 +1,16 @@
+<<<<<<< HEAD
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
 #nullable disable
+=======
+﻿using Microsoft.EntityFrameworkCore;
+>>>>>>> parent of af51969 (Add Entities using scaffolding)
 
 namespace DIMS_Core.DataAccessLayer.Models
 {
-    public partial class DimsCoreContext : DbContext
+    public class DimsCoreContext : DbContext
     {
         public DimsCoreContext()
         {
@@ -18,238 +22,113 @@ namespace DIMS_Core.DataAccessLayer.Models
         }
 
         public virtual DbSet<Direction> Directions { get; set; }
-        public virtual DbSet<Task> Tasks { get; set; }
-        public virtual DbSet<TaskState> TaskStates { get; set; }
-        public virtual DbSet<TaskTrack> TaskTracks { get; set; }
-        public virtual DbSet<UserProfile> UserProfiles { get; set; }
-        public virtual DbSet<UserTask> UserTasks { get; set; }
-        public virtual DbSet<VTask> VTasks { get; set; }
-        public virtual DbSet<VUserProfile> VUserProfiles { get; set; }
-        public virtual DbSet<VUserProgress> VUserProgresses { get; set; }
-        public virtual DbSet<VUserTask> VUserTasks { get; set; }
-        public virtual DbSet<VUserTrack> VUserTracks { get; set; }
 
+        public virtual DbSet<UserProfile> UserProfiles { get; set; }
+
+<<<<<<< HEAD
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             
         }
+=======
+        public virtual DbSet<VUserProfile> VUserProfiles { get; set; }
+>>>>>>> parent of af51969 (Add Entities using scaffolding)
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
-
             modelBuilder.Entity<Direction>(entity =>
-            {
-                entity.Property(e => e.Description).HasMaxLength(250);
+                                           {
+                                               entity.Property(e => e.Description)
+                                                     .HasMaxLength(250);
 
-                entity.Property(e => e.Name)
-                    .IsRequired()
-                    .HasMaxLength(50);
-            });
-
-            modelBuilder.Entity<Task>(entity =>
-            {
-                entity.Property(e => e.DeadlineDate).HasColumnType("date");
-
-                entity.Property(e => e.Description).HasMaxLength(250);
-
-                entity.Property(e => e.Name)
-                    .IsRequired()
-                    .HasMaxLength(50);
-
-                entity.Property(e => e.StartDate).HasColumnType("date");
-            });
-
-            modelBuilder.Entity<TaskState>(entity =>
-            {
-                entity.HasKey(e => e.StateId)
-                    .HasName("PK_TaskStates_StateId");
-
-                entity.Property(e => e.StateName)
-                    .IsRequired()
-                    .HasMaxLength(50);
-            });
-
-            modelBuilder.Entity<TaskTrack>(entity =>
-            {
-                entity.Property(e => e.TrackDate).HasColumnType("date");
-
-                entity.Property(e => e.TrackNote).HasMaxLength(250);
-
-                entity.HasOne(d => d.UserTask)
-                    .WithMany(p => p.TaskTracks)
-                    .HasForeignKey(d => d.UserTaskId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK__TaskTrack__UserT__32E0915F");
-            });
+                                               entity.Property(e => e.Name)
+                                                     .IsRequired()
+                                                     .HasMaxLength(50);
+                                           });
 
             modelBuilder.Entity<UserProfile>(entity =>
-            {
-                entity.HasKey(e => e.UserId)
-                    .HasName("PK_UserProfiles_UserId");
+                                             {
+                                                 entity.HasKey(e => e.UserId)
+                                                       .HasName("PK__UserProf__1788CC4C01393682");
 
-                entity.Property(e => e.Address).HasMaxLength(120);
+                                                 entity.Property(e => e.Address)
+                                                       .HasMaxLength(120);
 
-                entity.Property(e => e.BirthDate).HasColumnType("date");
+                                                 entity.Property(e => e.BirthDate)
+                                                       .HasColumnType("date");
 
-                entity.Property(e => e.Education).HasMaxLength(50);
+                                                 entity.Property(e => e.Education)
+                                                       .HasMaxLength(50);
 
-                entity.Property(e => e.Email)
-                    .IsRequired()
-                    .HasMaxLength(50);
+                                                 entity.Property(e => e.Email)
+                                                       .IsRequired()
+                                                       .HasMaxLength(50);
 
-                entity.Property(e => e.FirstName)
-                    .IsRequired()
-                    .HasMaxLength(50);
+                                                 entity.Property(e => e.FirstName)
+                                                       .IsRequired()
+                                                       .HasMaxLength(50);
 
-                entity.Property(e => e.LastName)
-                    .IsRequired()
-                    .HasMaxLength(50);
+                                                 entity.Property(e => e.LastName)
+                                                       .IsRequired()
+                                                       .HasMaxLength(50);
 
-                entity.Property(e => e.MobilePhone)
-                    .IsRequired()
-                    .HasMaxLength(50);
+                                                 entity.Property(e => e.MobilePhone)
+                                                       .IsRequired()
+                                                       .HasMaxLength(50);
 
-                entity.Property(e => e.Skype).HasMaxLength(50);
+                                                 entity.Property(e => e.Skype)
+                                                       .HasMaxLength(50);
 
-                entity.Property(e => e.StartDate).HasColumnType("date");
+                                                 entity.Property(e => e.StartDate)
+                                                       .HasColumnType("date");
 
-                entity.HasOne(d => d.Direction)
-                    .WithMany(p => p.UserProfiles)
-                    .HasForeignKey(d => d.DirectionId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK__UserProfi__Direc__267ABA7A");
-            });
-
-            modelBuilder.Entity<UserTask>(entity =>
-            {
-                entity.HasOne(d => d.State)
-                    .WithMany(p => p.UserTasks)
-                    .HasForeignKey(d => d.StateId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK__UserTasks__State__300424B4");
-
-                entity.HasOne(d => d.Task)
-                    .WithMany(p => p.UserTasks)
-                    .HasForeignKey(d => d.TaskId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK__UserTasks__TaskI__2E1BDC42");
-
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.UserTasks)
-                    .HasForeignKey(d => d.UserId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK__UserTasks__UserI__2F10007B");
-            });
-
-            modelBuilder.Entity<VTask>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("vTasks");
-
-                entity.Property(e => e.DeadlineDate).HasColumnType("date");
-
-                entity.Property(e => e.Description).HasMaxLength(250);
-
-                entity.Property(e => e.Name)
-                    .IsRequired()
-                    .HasMaxLength(50);
-
-                entity.Property(e => e.StartDate).HasColumnType("date");
-
-                entity.Property(e => e.TaskId).ValueGeneratedOnAdd();
-            });
+                                                 entity.HasOne(d => d.Direction)
+                                                       .WithMany(p => p.UserProfile)
+                                                       .HasForeignKey(d => d.DirectionId)
+                                                       .OnDelete(DeleteBehavior.ClientSetNull)
+                                                       .HasConstraintName("FK__UserProfi__Direc__38996AB5");
+                                             });
 
             modelBuilder.Entity<VUserProfile>(entity =>
-            {
-                entity.HasNoKey();
+                                              {
+                                                  entity.HasNoKey();
 
-                entity.ToView("vUserProfiles");
+                                                  entity.ToView("vUserProfiles");
 
-                entity.Property(e => e.Address).HasMaxLength(120);
+                                                  entity.Property(e => e.Address)
+                                                        .HasMaxLength(120);
 
-                entity.Property(e => e.Direction)
-                    .IsRequired()
-                    .HasMaxLength(50);
+                                                  entity.Property(e => e.Direction)
+                                                        .IsRequired()
+                                                        .HasMaxLength(50);
 
-                entity.Property(e => e.Education).HasMaxLength(50);
+                                                  entity.Property(e => e.Education)
+                                                        .HasMaxLength(50);
 
-                entity.Property(e => e.Email)
-                    .IsRequired()
-                    .HasMaxLength(50);
+                                                  entity.Property(e => e.Email)
+                                                        .IsRequired()
+                                                        .HasMaxLength(50);
 
-                entity.Property(e => e.FullName)
-                    .IsRequired()
-                    .HasMaxLength(101);
+                                                  entity.Property(e => e.FullName)
+                                                        .IsRequired()
+                                                        .HasMaxLength(101);
 
-                entity.Property(e => e.MobilePhone)
-                    .IsRequired()
-                    .HasMaxLength(50);
+                                                  entity.Property(e => e.MobilePhone)
+                                                        .IsRequired()
+                                                        .HasMaxLength(50);
 
-                entity.Property(e => e.Skype).HasMaxLength(50);
+                                                  entity.Property(e => e.Skype)
+                                                        .HasMaxLength(50);
 
-                entity.Property(e => e.StartDate).HasColumnType("date");
-            });
-
-            modelBuilder.Entity<VUserProgress>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("vUserProgress");
-
-                entity.Property(e => e.TaskName)
-                    .IsRequired()
-                    .HasMaxLength(50);
-
-                entity.Property(e => e.TrackDate).HasColumnType("date");
-
-                entity.Property(e => e.TrackNote).HasMaxLength(250);
-
-                entity.Property(e => e.UserName)
-                    .IsRequired()
-                    .HasMaxLength(50);
-            });
-
-            modelBuilder.Entity<VUserTask>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("vUserTasks");
-
-                entity.Property(e => e.DeadlineDate).HasColumnType("date");
-
-                entity.Property(e => e.Description).HasMaxLength(250);
-
-                entity.Property(e => e.StartDate).HasColumnType("date");
-
-                entity.Property(e => e.StateName)
-                    .IsRequired()
-                    .HasMaxLength(50);
-
-                entity.Property(e => e.TaskName)
-                    .IsRequired()
-                    .HasMaxLength(50);
-            });
-
-            modelBuilder.Entity<VUserTrack>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("vUserTracks");
-
-                entity.Property(e => e.TaskName)
-                    .IsRequired()
-                    .HasMaxLength(50);
-
-                entity.Property(e => e.TrackDate).HasColumnType("date");
-
-                entity.Property(e => e.TrackNote).HasMaxLength(250);
-            });
+                                                  entity.Property(e => e.StartDate)
+                                                        .HasColumnType("date");
+                                              });
 
             OnModelCreatingPartial(modelBuilder);
         }
 
-        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+        private void OnModelCreatingPartial(ModelBuilder modelBuilder)
+        {
+        }
     }
 }
