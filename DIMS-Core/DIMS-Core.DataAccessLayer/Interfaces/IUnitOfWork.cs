57392850e067bc4b01e4a774abf881d266d95a11
dzmitri-litvinov <dzmitri.litvinov@gmail.com>
2021-07-01 21:53:51 +0300

    Add Alias Threading

--- DIMS-Core/DIMS-Core.DataAccessLayer/Interfaces/IUnitOfWork.cs
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Interfaces/IUnitOfWork.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Threading.Tasks;
+using System;
+using AliasThreading = System.Threading.Tasks;
 using DIMS_Core.DataAccessLayer.Models;
 
 namespace DIMS_Core.DataAccessLayer.Interfaces
@@ -12,6 +12,6 @@ namespace DIMS_Core.DataAccessLayer.Interfaces
 
         IReadOnlyRepository<VUserProfile> VUserProfileRepository { get; }
 
-        Task Save();
+        AliasThreading.Task Save();
     }
 }
--- DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/UnitOfWork.cs
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/UnitOfWork.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+using AliasThreading = System.Threading.Tasks;
 using DIMS_Core.DataAccessLayer.Interfaces;
 using DIMS_Core.DataAccessLayer.Models;
 
@@ -38,7 +38,7 @@ namespace DIMS_Core.DataAccessLayer.Repositories
         ///     Repository.
         /// </summary>
         /// <returns></returns>
-        public Task Save()
+        public AliasThreading.Task Save()
         {
             return _context.SaveChangesAsync();
         }
